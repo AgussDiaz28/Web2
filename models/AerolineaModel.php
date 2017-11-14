96c0f50b2a9ec965e0cd2283bcc847b9c31722da
Agustin Diaz Gargiulo <agussdiaz28@gmail.com>
2017-11-14 20:56:48 -0300

    Orden ascendente de categorias

--- models/AerolineaModel.php
+++ models/AerolineaModel.php
@@ -3,30 +3,32 @@ include_once 'models/dbModel.php';
 class AerolineaModel extends dbModel
 {
 
-  public function getAerolineas(){
-    $sentencia = $this->db->prepare( "SELECT * FROM Aerolineas");
-    $sentencia->execute();
-    return $sentencia->fetchAll(PDO::FETCH_ASSOC);
-  }
+    public function getAerolineas()
+    {
+        $sentencia = $this->db->prepare( "SELECT * FROM Aerolineas ORDER BY NOMBRE_AEROLINEA ASC");
+        $sentencia->execute();
+        return $sentencia->fetchAll(PDO::FETCH_ASSOC);
+    }
 
   // ABM Aerolineas //
 
-  public function borrarAerolinea($id){
-    $sentencia = $this->db->prepare( "DELETE FROM Aerolineas WHERE ID_AEROLINEA = ?");
-    $sentencia->execute($id);
-  }
+    public function borrarAerolinea($id)
+    {
+        $sentencia = $this->db->prepare( "DELETE FROM Aerolineas WHERE ID_AEROLINEA = ?");
+        $sentencia->execute($id);
+    }
 
-  public function agregarAerolinea($datos){
-    $sentencia = $this->db->prepare( "INSERT INTO Aerolineas(NOMBRE_AEROLINEA,PAIS_ORIGEN,CANT_AVIONES) VALUES (?,?,?) ");
-    $sentencia->execute($datos);
-  }
+    public function agregarAerolinea($datos)
+    {
+        $sentencia = $this->db->prepare( "INSERT INTO Aerolineas(NOMBRE_AEROLINEA,PAIS_ORIGEN,CANT_AVIONES) VALUES (?,?,?) ");
+        $sentencia->execute($datos);
+    }
 
-  public function updateAerolinea($datos){
-    $sentencia = $this->db->prepare( "UPDATE Aerolineas SET NOMBRE_AEROLINEA = ? , PAIS_ORIGEN = ? , CANT_AVIONES =? WHERE ID_AEROLINEA = ? ");
-    $sentencia->execute($datos);
-  }
+    public function updateAerolinea($datos)
+    {
+        $sentencia = $this->db->prepare( "UPDATE Aerolineas SET NOMBRE_AEROLINEA = ? , PAIS_ORIGEN = ? , CANT_AVIONES =? WHERE ID_AEROLINEA = ? ");
+        $sentencia->execute($datos);
+    }
 
   // ABM Aerolineas //
 }
-
- ?>
--- models/CiudadModel.php
+++ models/CiudadModel.php
@@ -2,32 +2,32 @@
 include_once 'models/dbModel.php';
 class CiudadModel extends dbModel
 {
-    public function getCiudades(){
-      $sentencia = $this->db->prepare( "SELECT * FROM Ciudades");
-      $sentencia->execute();
-      return $sentencia->fetchAll(PDO::FETCH_ASSOC);
+    public function getCiudades()
+    {
+        $sentencia = $this->db->prepare( "SELECT * FROM Ciudades ORDER BY NOMBRE_CIUDAD ASC");
+        $sentencia->execute();
+        return $sentencia->fetchAll(PDO::FETCH_ASSOC);
     }
 
     // ABM Ciudades //
 
-    public function borrarCiudad($id){
-      $sentencia = $this->db->prepare( "DELETE FROM ciudades WHERE ID_CIUDAD = ?");
-      $sentencia->execute($id);
+    public function borrarCiudad($id)
+    {
+        $sentencia = $this->db->prepare( "DELETE FROM ciudades WHERE ID_CIUDAD = ?");
+        $sentencia->execute($id);
     }
 
-    public function agregarCiudad($nombre){
-      $sentencia = $this->db->prepare( "INSERT INTO ciudades(NOMBRE_CIUDAD) VALUES (?) ");
-      $sentencia->execute($nombre);
+    public function agregarCiudad($nombre)
+    {
+        $sentencia = $this->db->prepare( "INSERT INTO ciudades(NOMBRE_CIUDAD) VALUES (?) ");
+        $sentencia->execute($nombre);
     }
 
-    public function actualizarCiudad($values){
-      $sentencia = $this->db->prepare( "UPDATE ciudades SET NOMBRE_CIUDAD = ? WHERE ID_CIUDAD = ? ");
-      $sentencia->execute($values);
+    public function actualizarCiudad($values)
+    {
+        $sentencia = $this->db->prepare( "UPDATE ciudades SET NOMBRE_CIUDAD = ? WHERE ID_CIUDAD = ? ");
+        $sentencia->execute($values);
     }
 
     // ABM Ciudades //
 }
-
-
-
-?>
