ce6603f1b0ddff2f4770cb647c30093d121ab7e6
Agustin Diaz Gargiulo <agussdiaz28@gmail.com>
2018-06-01 17:49:04 -0300

    multiple db coneccion

--- dbconfig.php
+++ dbconfig.php
@@ -1,8 +1,14 @@
 <?php
-
+//CONFIGURACION DE BASE MYSQL
 define('DB','turismo');
 define('HOST','localhost');
 define('USERNAME','root');
 define('PASSWORD','root');
+
+//CONFIGURACION DE BASE POSTGRESS
+define('DBP','public');
+define('HOSTP','localhost');
+define('USERNAMEP','postgres');
+define('PASSWORDP','root');
 
  ?>
--- models/dbModel.php
+++ models/dbModel.php
@@ -3,30 +3,57 @@ require_once('dbconfig.php');
 class dbModel{
 
   protected $db;
+  protected $dbp;
   protected $view;
+
+    private function constructMysql(){ // Contruyo mi base MySqL
+        $host = HOST;
+        $dbn = DB;
+
+        $config = "mysql:host=$host;charset=utf8";
+        $this->db = new PDO($config, USERNAME, PASSWORD);
+
+        $this->db->exec('CREATE DATABASE IF NOT EXISTS '.$dbn);
+        $this->db->exec('USE '. $dbn);
+
+        $query = $this->db->exec( "SELECT * FROM usuario");
+
+        $sql = file_get_contents('db.sql');
+
+        if (!$query){
+            $this->db->exec($sql);
+            $config = "mysql:host=$host;dbname=$dbn;charset=utf8";
+            $this->db = new PDO($config, USERNAME, PASSWORD);
+        }
+    }
+
+    // Coneccion con la base postgres
+    private function constructPMySql(){
+        $host = HOSTP;
+        $dbn = DBP;
+
+        $config = "pgsql:host=$host;charset=utf8";
+        $this->dbp = new PDO($config, USERNAMEP, PASSWORDP);
+
+        $this->dbp->exec('CREATE DATABASE IF NOT EXISTS '.$dbn);
+        $this->dbp->exec('USE '. $dbn);
+
+        $query = $this->dbp->exec( "SELECT * FROM usuario");
+
+        $sqlPostgress = file_get_contents('Reservas_create.sql');
+
+        if (!$query){
+            $this->dbp->exec($sqlPostgress);
+            $config = "mysql:host=$host;dbname=$dbn;charset=utf8";
+            $this->dbp = new PDO($config, USERNAMEP, PASSWORDP);
+        }
+    }
 
   function __construct(){
     try{
 
-      $host = HOST;
-      $dbn = DB;
-
-      $config = "mysql:host=$host;charset=utf8";
-      $this->db = new PDO($config, USERNAME, PASSWORD);
-
-      $this->db->exec('CREATE DATABASE IF NOT EXISTS '.$dbn);
-      $this->db->exec('USE '. $dbn);
-
-      $query = $this->db->exec( "SELECT * FROM usuario");
-
-      $sql = file_get_contents('db.sql');
-      $sqlPostgress = file_get_contents('Reservas_create.sql');
-
-      if (!$query){
-          $this->db->exec($sql);
-          $config = "mysql:host=$host;dbname=$dbn;charset=utf8";
-          $this->db = new PDO($config, USERNAME, PASSWORD);
-      }
+        $this->constructMysql();
+        $this->constructPMySql();
 
     }catch (Exception $e) {
 
