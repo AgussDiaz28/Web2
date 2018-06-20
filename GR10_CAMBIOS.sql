c17115f02ded2429cad76139d073aa5d31afe5f2
Agustin Diaz Gargiulo <agussdiaz28@gmail.com>
2018-06-20 17:23:46 -0300

    Avances en el TP Especial

--- GR10_CAMBIOS.sql
+++ GR10_CAMBIOS.sql
@@ -42,7 +42,6 @@ insert into gr10_tipo_doc values(5,'LC');
 insert into gr10_persona values (1,38524932,'Diaz','Agustin','1995-01-28','agussdiaz28@gmail.com');
 insert into gr10_persona values (2,42908673,'Diaz Gargiulo','Mariano','1999-08-31','marianodiaz@gmail.com');
 insert into gr10_persona values (3,13244453,'Diaz','Ricardo','1959-11-01','rjdiazt@gmail.com');
-
 insert into gr10_persona values (1,27890165,'Pizzi','Alejandra','1979-08-11','alejandra@qwavee.com');
 insert into gr10_persona values (1,25908361,'Rodriguez','Diego','1977-01-17','diego@qwavee.com');
 
@@ -94,11 +93,12 @@ insert into gr10_huesped values (1,25908361);
 
 
 /* inserts de reserva */
-insert into gr10_reserva values (1,'2017-10-01','2018-01-01','2018-01-24','TIPO',3,1600,1,1,38524932);
-insert into gr10_reserva values (2,'2017-11-30','2018-02-01','2018-03-14','TIPO',4,800,1,2,42908673);
-insert into gr10_reserva values (3,'2017-09-18','2018-04-01','2018-04-10','TIPO',5,600,1,3,13244453);
+insert into gr10_reserva values (1,'2017-10-01','2018-01-01','2018-01-24','TIPO',4,1600,1,1,38524932);
+insert into gr10_reserva values (2,'2017-11-30','2018-02-01','2018-03-14','TIPO',1,800,1,2,42908673);
+insert into gr10_reserva values (3,'2017-09-18','2018-04-01','2018-04-10','TIPO',2,600,1,3,13244453);
 insert into gr10_reserva values (4,'2017-10-14','2018-01-01','2018-01-14','TIPO',2,500,1,1,27890165);
 insert into gr10_reserva values (5,'2017-12-12','2018-05-01','2018-05-8','TIPO',1,450,1,1,25908361);
+
 
 /* insert huesped reserva */
 insert into gr10_huesped_reserva values(1,38524932,1);
@@ -153,8 +153,6 @@ insert into gr10_habitacion values (5,13,0,1,1,true,1,1,true,1,true);
 
 /* Punto a. Que las fechas de las reservas sean consistentes, es decir que la fecha de inicio de la reserva sea menor que la fecha de finalización. */
 
-select * from gr10_reserva;
-
 alter table gr10_reserva add constraint gr10_reserva_check_fecha_reserva check ( fecha_reserva <= now());
 alter table gr10_reserva add check (fecha_reserva <= fecha_desde);
 alter table gr10_reserva add check ( fecha_desde < fecha_hasta);
@@ -169,29 +167,46 @@ alter table gr10_reserva add check ( fecha_desde < fecha_hasta);
 
 /* Punto b. Que el detalle de las habitaciones sea consistente con el tipo de departamento, es decir que si el tipo de departamento es de 2 habitaciones, en el detalle se consideren como máximo 2 habitaciones. */
 
-/* CONSULTA SOBRE LA FORMA DE HACERLO, SI CON UN COUNT DE HABITACIONES O AGREGAR UNA COLUMNA A LA TABLA DE DEPARTAMENTO Y COMPARAR CON TIPO_DEPTO
+create or replace function trfn_gr10_check_depto_habitaciones () returns trigger AS $body$
+declare
+v_cant_habitaciones_tipo integer;
+v_count_habitaciones integer;
+begin
+	select gr10_tipo_dpto.cant_habitaciones
+	into v_cant_habitaciones_tipo
+	from gr10_departamento
+	join gr10_tipo_dpto on (gr10_departamento.id_tipo_depto = gr10_tipo_dpto.id_tipo_depto )
+	where id_dpto = new.id_dpto;
+	
+	select count(*) 
+	into v_count_habitaciones 
+	from gr10_habitacion
+	where id_dpto = new.id_dpto;	
 
-select * from gr10_departamento; 
-select * from gr10_tipo_dpto;
+	IF (v_cant_habitaciones_tipo <> v_count_habitaciones) THEN
+            RAISE EXCEPTION 'La cantidad de habitaciones no coinciden con las del tipo del depto';
+    END IF;
+    RETURN NEW;
+end;
+$body$ LANGUAGE plpgsql;
 
-select * from gr10_habitacion;
+create  trigger tr_gr10_departamento_habitaciones
+before insert or update on gr10_departamento
+for each statement
+execute procedure trfn_gr10_check_depto_habitaciones();
 
-alter table gr10_departamento add constraint gr10_departamento_check_cant_habitaciones
-*/
+create  trigger tr_gr10_habitaciones_departamento
+before insert or update on gr10_habitacion
+for each statement
+execute procedure trfn_gr10_check_depto_habitaciones();
+
 
 /* Punto c. Que tanto la persona que realiza la reserva como los huéspedes no sea el propietario del departamento  */
 
-
-select *
-from gr10_reserva ;
-join gr10_departamento on (gr10_departamento.id_dpto = gr10_reserva.id_dpto);
-
-select * from gr10_huesped_reserva;
-
 create or replace function trfn_gr10_check_propietario_no_alquila () returns trigger AS $body$
-BEGIN
+begin
     IF ((SELECT COUNT(*)
-        FROM gr01_reserva r
+        FROM gr10_reserva r
         LEFT JOIN gr10_huesped_reserva hr ON r.id_reserva = hr.id_reserva
         WHERE r.id_dpto IN (SELECT d.id_dpto
                             FROM gr10_departamento d
@@ -200,17 +215,120 @@ BEGIN
             RAISE EXCEPTION 'EL propietario del departamento no puede ser huesped del mismo';
     END IF;
     RETURN NEW;
-END;
+end;
 $body$ LANGUAGE plpgsql;
 
-create  trigger gr10_reserva_departamento_persona 
+create  trigger tr_gr10_reserva_departamento_persona 
 before insert or update on gr10_reserva
 for each statement
 execute procedure trfn_gr10_check_propietario_no_alquila();
 
-
 /* Punto d. Que la cantidad de huéspedes no supere la cantidad máxima de personas permitidas para una reserva */
 
+create or replace function trfn_gr10_check_cantidad_huespedes () returns trigger AS $body$
+declare
+v_cantidad_huesped integer;
+v_capacidad_depto integer;
+begin
+	select count(nro_doc) into v_cantidad_huesped  from gr10_huesped_reserva where id_reserva = new.id_reserva;
+	
+	select gr10_tipo_dpto.cant_habitaciones
+	into v_capacidad_depto
+	from gr10_departamento
+	join gr10_tipo_dpto on (gr10_departamento.id_tipo_depto = gr10_tipo_dpto.id_tipo_depto )
+	join gr10_reserva on (gr10_reserva.id_dpto = gr10_departamento.id_dpto)
+	where gr10_reserva.id_reserva = new.id_reserva;
 
+    IF (v_capacidad_depto < v_cantidad_huesped) THEN
+            RAISE EXCEPTION 'La capacidad del departamento fue superado.';
+    END IF;
+    RETURN NEW;
+end;
+$body$ LANGUAGE plpgsql;
 
+create  trigger tr_gr10_reserva_departamento_huespedes
+before insert or update on gr10_reserva
+for each statement
+execute procedure trfn_gr10_check_cantidad_huespedes();
+
+/* Creacion de Servicios */
+
+/* a) dada una fecha listar departamentos con su estado, es decir, si estan disponibles u ocupados en esa fecha */
+
+CREATE OR REPLACE FUNCTION FN_GR10_listar_estado_departamento(fecha DATE)
+RETURNS TABLE(id_departamento INT, estado_dpto VARCHAR) AS $body$
+DECLARE 
+    var_r RECORD;
+BEGIN
+    FOR var_r IN (SELECT d.id_dpto,r.id_reserva
+                  FROM gr10_departamento d
+                  LEFT JOIN gr10_reserva r ON d.id_dpto = r.id_dpto
+                  AND fecha BETWEEN r.fecha_desde AND r.fecha_hasta
+                  ORDER BY d.id_dpto)
+    LOOP
+        id_departamento := var_r.id_dpto;
+        IF (var_r.id_reserva IS NOT NULL) THEN
+            estado_dpto := 'ocupado';
+        ELSE
+            estado_dpto := 'libre';
+        END IF;
+        RETURN NEXT;
+    END LOOP;    
+END;
+$body$ LANGUAGE plpgsql;
+
+/* b) listar todos los departamentos que, dado un rango de fecha, estan libres en dicho rango */
+
+CREATE OR REPLACE FUNCTION FN_GR10_departametos_disponibles(f_desde DATE, f_hasta DATE, city varchar(50))
+RETURNS TABLE(id_departamento INT) AS $body$
+DECLARE 
+    var_r RECORD;
+	v_id_ciudad integer;
+begin
+	
+	select id_ciudad into v_id_ciudad from gr10_ciudad where nombre like city;
+
+    IF (f_desde < f_hasta) THEN
+        FOR var_r IN (SELECT d.id_dpto
+                      FROM gr10_departamento d
+                      LEFT JOIN gr10_reserva r ON d.id_dpto = r.id_dpto
+                      AND ((r.fecha_desde NOT BETWEEN f_desde AND f_hasta) AND (r.fecha_hasta NOT BETWEEN f_desde AND f_hasta))
+                      WHERE id_ciudad = v_id_ciudad
+                      GROUP BY d.id_dpto
+                      ORDER BY d.id_dpto)
+        LOOP
+            id_departamento := var_r.id_dpto;
+            RETURN NEXT;
+        END LOOP;
+    ELSE
+        RAISE EXCEPTION 'La fecha de inicio debe ser menor a la fecha de fin';
+    END IF;
+END;
+$body$ LANGUAGE plpgsql;
+
+/* Creacion de vistas */
+
+/* Punto a. listar departamentos y su recaudacion en los ultimos 6 meses */
+
+create or replace view GR10_dpto_recaudacion AS 
+    SELECT d.id_dpto AS "ID departamento", COALESCE(SUM(p.importe), 0) AS "recaudacion"
+    FROM gr10_departamento d
+    LEFT JOIN gr10_reserva r ON d.id_dpto = r.id_dpto
+    LEFT JOIN gr10_pago p ON r.id_reserva = p.id_reserva
+        AND p.fecha_pago > CURRENT_DATE - INTERVAL '6 months'
+    GROUP BY d.id_dpto
+    ORDER BY d.id_dpto;
+	
+/* Punto b. listar departamentos ordenados por ciudad y por mejor rating */	
+
+create or replace view GR10_dpto_rating AS
+	SELECT d.id_dpto, d.id_ciudad, COALESCE(AVG(CAST(c.estrellas AS FLOAT)), 0)
+	FROM gr10_departamento d
+	LEFT join gr10_reserva r ON d.id_dpto = r.id_dpto
+	LEFT JOIN gr10_comentario c ON r.id_reserva = c.id_reserva
+	GROUP BY d.id_dpto
+	ORDER BY 
+		d.id_ciudad,
+		AVG(c.estrellas);
+--- 
 
